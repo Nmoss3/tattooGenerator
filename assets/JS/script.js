246c9a23e99c541ed6b4fdf59c37bacf8e91d99d
Nmoss3 <75024930+Nmoss3@users.noreply.github.com>
2021-03-16 19:06:31 -0700

    flickr now shows images

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,8 +1,11 @@
 var ideaInputEl = $("#mySearchField1");
 var ideaInputE2 = $("#mySearchField2");
 var tattooContainerEl = $("#tattoos-container");
+var searchFlickr = $("searchFlickr");
+var searchPinterest = $("#SearchPinterest");
 //var tattooSearchTerm = document.querySelector("#tattoo-search-term");
 var searchButton = $("#searchFlickr");
+//t_url = "http://farm" + photo.farm + ".static.flickr.com/" + photo.server + "/" + photo.id + "_" + photo.secret + "_" + "t.jpg";
 $(document).ready(function () {
   searchButton.on("click", function () {
     var searchTerm = ideaInputEl.val();
@@ -10,88 +13,56 @@ $(document).ready(function () {
     getUserTattoos(searchTerm);
   });
 });
+function photoURL(data) {
+  $("#tattoo1").html("");
+  $("#tattoo2").html("");
+  $("#tattoo3").html("");
+  for (var i = 0; i < 6; i++) {
+    var photos = data.photos.photo;
+    var photo = photos[i];
 
-var getUserTattoos = function (user) {
+    var myURL =
+      "http://farm" +
+      photo.farm +
+      ".static.flickr.com/" +
+      photo.server +
+      "/" +
+      photo.id +
+      "_" +
+      photo.secret +
+      "_" +
+      "t.jpg";
+
+    if (i < 2) {
+      var imgTag = $("<img />");
+      imgTag.attr({ src: myURL }).width(405).height(300);
+      $("#tattoo1").append(imgTag);
+    } else if (i > 1 && i < 4) {
+      var imgTag = $("<img />");
+      imgTag.attr({ src: myURL }).width(405).height(300);
+      $("#tattoo2").append(imgTag);
+    } else {
+      var imgTag = $("<img />");
+      imgTag.attr({ src: myURL }).width(405).height(300);
+      $("#tattoo3").append(imgTag);
+    }
+  }
+}
+var getUserTattoos = function (searchTerm) {
   // format the Flickr api url
   var apiUrl =
-    "https://api.flickr.com/services/rest/?method=flickr.photos.getRecent&api_key=36cfa3d828dec55a60e1961894c998ed&per_page=10&format=json&nojsoncallback=1";
-
-  // make a get request to url
-  fetch(apiUrl)
-    .then(function (response) {
-      // request was successful
-      if (response.ok) {
-        console.log(response);
-        response.json().then(function (data) {
-          console.log(data);
-          displayTattoos(data, user);
-        });
-      }
-    })
-    .catch(function (error) {});
-};
-
-var getFeaturedTattoos = function (language) {
-  // format the Pinterest api url
-  var apiUrl = "https://api.pinterest.com/search/tattoos?q=";
+    "https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=a035d2026d37e843cf4a59c8fdc80b9d&tags=" +
+    searchTerm +
+    "&per_page=&format=json&nojsoncallback=1";
 
   // make a get request to url
   fetch(apiUrl).then(function (response) {
     // request was successful
     if (response.ok) {
+      console.log(response);
       response.json().then(function (data) {
-        displayTattoos(data.items, language);
+        photoURL(data);
       });
     }
   });
 };
-
-var searchButton = $("#searchFlickr");
-$(document).ready(function () {
-  searchButton.on("click", function () {
-    var searchTerm = ideaInputE2.val();
-    console.log(searchTerm);
-    getUserTattoos(searchTerm);
-  });
-});
-
-var displayTattoos = function (tattoos, searchTerm) {
-  // check if api returned any images
-  if (tattoos.length === 0) {
-    tattooContainerEl.textContent = "";
-    return;
-  }
-
-  repoSearchTerm.textContent = searchTerm;
-
-  // loop over images
-  for (var i = 0; i < repos.length; i++) {
-    // format image name
-    var repoName = tattoos[i].owner.login + "/" + tattoos[i].name;
-
-    // create a link for each tattoo pic
-    var tattooEl = document.createElement("a");
-    tattooEl.classList =
-      "list-item flex-row justify-space-between align-center";
-    tattooEl.setAttribute("href", "./single-repo.html?repo=" + repoName);
-
-    // create a span element to hold tattoo info
-    var titleEl = document.createElement("span");
-    titleEl.textContent = tattooName;
-
-    // append to container
-    tattooEl.appendChild(titleEl);
-
-    // create a status element
-    var statusEl = document.createElement("span");
-    statusEl.classList = "flex-row align-center";
-
-    // append to container
-    tattooEl.appendChild(statusEl);
-
-    // append container to the dom
-    tattooContainerEl.appendChild(tattooEl);
-  }
-};
-
-// add event listeners to form and button container
